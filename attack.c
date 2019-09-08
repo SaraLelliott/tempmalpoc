6d85f1fcdf0b19fedac67acde1bae1d55edf824d
root <root@localhost.localdomain>
2019-09-08 15:19:42 +1000

    first!

---
+++ attack.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+static void malicious() __attribute__((constructor));
+
+void malicious() {
+    system("/usr/local/bin/score a87f9a76-2f9b-41a0-8d59-55ae4456e9ec");
+}
